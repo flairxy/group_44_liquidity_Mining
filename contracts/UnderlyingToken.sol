6ca3ffa2d5a9fd00863b1d60cf318142db2328cd
flairxy <okonkwo.miracle@raadaa.com>
2021-04-08 22:30:50 +0100

    description

--- contracts/UnderlyingToken.sol
+++ contracts/UnderlyingToken.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+//The underlying token is the base token you deposit to the liquidity address
+
 contract UnderlyingToken is ERC20 {
     constructor() ERC20("UTK", "Underlying Token") {}
 
